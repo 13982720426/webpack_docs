4976709590b92d5c754bc72e0e65df0f27b042e1
jackhoo_98 <jackhoo_98@foxmail.com>
2023-11-05 16:05:02 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:13982720426/webpack_docs.git master:gh-pages
+git push -f git@gitee.com:jackhoo_98/webpack_docs.git master:gh-pages
 
 cd -
